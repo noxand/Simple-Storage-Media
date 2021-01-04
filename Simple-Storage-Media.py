31d7318b825ef66a21f2a3570b2a1c69e1135c0f
Hardefa <hardefarogonondo@gmail.com>
2021-01-04 19:52:17 +0700

    first commit

---
+++ Simple-Storage-Media.py
@@ -0,0 +1,37 @@
+'''
+Homework Assignment #4: Lists
+By: Hardefa Rogonondo
+LinkedIn: https://www.linkedin.com/in/hardefa-rogonondo/
+GitHub: https://github.com/noxand
+'''
+
+# Initialization
+myUniqueList = []
+myLeftovers = []
+
+# Functions
+def AddtoList(add):
+    global myUniqueList, myLeftovers
+    if add in myUniqueList:
+        print("It's not unique, adding this to leftovers")
+        myLeftovers.append(add)
+    else:
+        myUniqueList.append(add)
+
+# Main
+print("My Unique List = ", myUniqueList)
+print("Let's add something unique to the list!")
+add = input("Type something to add = ")
+AddtoList(add)
+repeat = input("Do you want to add something again? (y/n) ")
+while repeat == "y":
+    add = input("Type something to add = ")
+    AddtoList(add)
+    repeat = input("Do you want to add something again? (y/n) ")
+    if repeat == "n":
+        break
+    elif repeat != "y" and repeat != "n":
+        print("Wrong Command!")
+        break    
+print("My New Unique List = ", myUniqueList)
+print("My Leftovers = ", myLeftovers)
